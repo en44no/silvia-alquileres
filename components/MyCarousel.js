cf17cb539dc0e0faeaa5ccbd4688984090410aee
NahuelMarquez <nahuel.marquez@gicla.com>
2023-01-05 15:23:34 -0300

    Fixes

--- components/MyCarousel.js
+++ components/MyCarousel.js
@@ -9,12 +9,11 @@ const MyCarousel = (props) => {
   const [currentSlide, setCurrentSlide] = React.useState(0)
   const [loaded, setLoaded] = React.useState(false)
   const [sliderRef, instanceRef] = useKeenSlider({
-    loop: true,
+    loop: false,
+    mode: "snap",
     rtl: false,
+    slides: { perView: "auto" },
     initial: 0,
-    slidesPerView: 1,
-    mode: "free-snap",
-    centered: true,
     slideChanged(slider) {
       setCurrentSlide(slider.track.details.rel)
     },
@@ -25,7 +24,7 @@ const MyCarousel = (props) => {
 
   return (
     <div className="navigation-wrapper">
-      <Box ref={sliderRef} className="keen-slider" style={{ minWidth: 600, maxWidth: 600, height: '100%' }} border='2px solid #ebeff5' borderRadius='20px'>
+      <Box ref={sliderRef} className="keen-slider" minW={{ sm: '0', md: 600, lg: 600, xl: 600 }} style={{ maxWidth: 600, height: '100%' }} border='2px solid #ebeff5' borderRadius='20px'>
 
         {imagesSrc.map((src, index = 1) =>
           <Box key={src} className={`keen-slider__slide number-slide${index}`} style={{ maxWidth: '100%', minWidth: '100%' }}>
@@ -42,11 +41,16 @@ const MyCarousel = (props) => {
               onClick={(e) =>
                 e.stopPropagation() || instanceRef.current?.prev()
               }
+              disabled={currentSlide === 0}
             />
 
             <Arrow
               onClick={(e) =>
                 e.stopPropagation() || instanceRef.current?.next()
+              }
+              disabled={
+                currentSlide ===
+                instanceRef.current.track.details.slides.length - 1
               }
             />
           </>
@@ -79,11 +83,12 @@ const MyCarousel = (props) => {
 }
 
 function Arrow(props) {
+  const disabeld = props.disabled ? " arrow--disabled" : ""
   return (
     <svg
       onClick={props.onClick}
       className={`arrow ${props.left ? "arrow--left" : "arrow--right"
-        }`}
+        } ${disabeld}`}
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
     >
